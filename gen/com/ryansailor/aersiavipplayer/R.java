0474a296aad234dacbee0b40c2b6124c80f3c691
Ryan Sailor <ryan@ryansailor.com>
2014-04-10 20:30:04 -0400

    Adjustments to UI
    The interface is closer to the traditional VIP style. Only requires a
    new application icon and changes to the actionbar icon coloring.

--- gen/com/ryansailor/aersiavipplayer/R.java
+++ gen/com/ryansailor/aersiavipplayer/R.java
@@ -11,14 +11,22 @@ public final class R {
     public static final class attr {
     }
     public static final class color {
-        public static final int music_controls_bg=0x7f040000;
-        public static final int music_controls_font=0x7f040001;
-        public static final int music_list_bg=0x7f040003;
-        public static final int music_list_item_bg=0x7f040004;
-        public static final int music_list_item_bg_sel=0x7f040006;
-        public static final int music_list_item_text=0x7f040005;
-        public static final int music_list_item_text_sel=0x7f040007;
-        public static final int nowplaying_font=0x7f040002;
+        public static final int actionbar_bg=0x7f040000;
+        public static final int actionbar_text=0x7f040001;
+        public static final int music_controls_bg=0x7f040002;
+        public static final int music_controls_font=0x7f040003;
+        public static final int music_list_bg=0x7f040005;
+        public static final int music_list_item_bg=0x7f040006;
+        public static final int music_list_item_bg_sel=0x7f040008;
+        public static final int music_list_item_text=0x7f040007;
+        public static final int music_list_item_text_sel=0x7f040009;
+        public static final int nowplaying_font=0x7f040004;
+        public static final int seekbar_background=0x7f04000e;
+        public static final int seekbar_empty=0x7f04000b;
+        public static final int seekbar_loaded=0x7f04000c;
+        public static final int seekbar_progress=0x7f04000d;
+        public static final int seekbar_thumb=0x7f04000a;
+        public static final int track_time_text=0x7f04000f;
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
@@ -36,6 +44,8 @@ public final class R {
         public static final int ic_action_play=0x7f020002;
         public static final int ic_action_previous=0x7f020003;
         public static final int ic_launcher=0x7f020004;
+        public static final int progressbar=0x7f020005;
+        public static final int seekbar_thumb=0x7f020006;
     }
     public static final class id {
         public static final int action_next=0x7f090005;
@@ -64,6 +74,8 @@ public final class R {
         public static final int music_time=0x7f060007;
     }
     public static final class style {
+        public static final int ActionBar=0x7f070002;
+        public static final int ActionBarTitle=0x7f070003;
         /** 
         Base application theme, dependent on API level. This theme is replaced
         by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
@@ -89,9 +101,9 @@ public final class R {
         /**  Application theme. 
          */
         public static final int AppTheme=0x7f070001;
-        public static final int Controls=0x7f070002;
-        public static final int MusicListItem=0x7f070004;
-        public static final int MusicListItemSelected=0x7f070005;
-        public static final int NowPlaying=0x7f070003;
+        public static final int Controls=0x7f070004;
+        public static final int MusicListItem=0x7f070006;
+        public static final int MusicListItemSelected=0x7f070007;
+        public static final int NowPlaying=0x7f070005;
     }
 }
